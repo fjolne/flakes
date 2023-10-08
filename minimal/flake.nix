317a61e8dc98a1f9a63d760efe8699dfac7bdd38
Oleg Martynov <fjolne.yngling@gmail.com>
2023-10-08 12:22:05 +0100

    minimal: new template

---
+++ minimal/flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "Minimal template";
+
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, poetry2nix }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        packages = { };
+
+        devShells = {
+          default = pkgs.mkShell {
+            packages = [ ];
+            shellHook = ''
+              echo hello
+            '';
+          };
+        };
+
+        formatter = pkgs.nixpkgs-fmt;
+      });
+}
