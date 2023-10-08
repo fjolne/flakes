3473b5ea368b5eb753451ea9abd4b208f1567313
Oleg Martynov <fjolne.yngling@gmail.com>
2023-10-08 12:21:06 +0100

    kaggle: use nixpkgs-unstable

--- kaggle/flake.nix
+++ kaggle/flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     poetry2nix = {
       url = "github:nix-community/poetry2nix";
       inputs.nixpkgs.follows = "nixpkgs";
