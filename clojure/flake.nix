0d1ee149f225053e7e592c9ccf85c7732dc6115c
Oleg Martynov <fjolne.yngling@gmail.com>
2023-11-25 21:08:33 +0000

    added clojure

---
+++ clojure/flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "Clojure template";
+
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells = {
+          default = pkgs.mkShell {
+            packages = with pkgs;
+              [
+                jdk21
+                clojure
+                clojure-lsp
+                babashka
+                just
+              ];
+          };
+        };
+      });
+}
