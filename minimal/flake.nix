cf58a66d6f390be00f67bb615d2966f5248c7ae0
Oleg Martynov <fjolne.yngling@gmail.com>
2024-01-19 19:33:18 +0000

    fixed minimal

--- minimal/flake.nix
+++ minimal/flake.nix
@@ -2,18 +2,16 @@
   description = "Minimal template";
 
   inputs = {
-    flake-utils.url = "github:numtide/flake-utils";
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils, poetry2nix }:
+  outputs = { self, nixpkgs, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
       in
       {
-        packages = { };
-
         devShells = {
           default = pkgs.mkShell {
             packages = [ ];
