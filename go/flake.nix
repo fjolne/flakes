8790ce4831a2adcd465b01cf3dd5b236c3b0eaca
Oleg Martynov <fjolne.yngling@gmail.com>
2024-02-18 10:29:24 +0000

    update go

--- go/flake.nix
+++ go/flake.nix
@@ -10,17 +10,26 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
+        lr = pkgs.buildGo122Module {
+          name = "lr";
+          src = ./.;
+          vendorHash = "sha256-7qQ5aOFIan5EpWiXtQdVza8nroP/qGKew48d1t+QeVY=";
+          CGO_ENABLED = 0;
+          doCheck = false;
+        };
       in
       {
+        packages.default = lr;
         devShells = {
           default = pkgs.mkShell {
             packages = with pkgs; [
-              go_1_20
+              go_1_22
               gopls # official language server
               gotools # goimports
               golangci-lint # linter
               delve # debugger
               go-tools # staticcheck
+              gore # repl
             ];
           };
         };
