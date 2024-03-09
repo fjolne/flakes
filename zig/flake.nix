796a55e0fa76540e42f8f77ee935ae8d9ca90b62
Oleg Martynov <fjolne.yngling@gmail.com>
2024-03-09 10:44:13 +0000

    zig: use 0.11.0

--- zig/flake.nix
+++ zig/flake.nix
@@ -2,7 +2,7 @@
   description = "Minimal template";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
     zig.url = "github:mitchellh/zig-overlay";
     flake-utils.url = "github:numtide/flake-utils";
   };
@@ -18,8 +18,9 @@
       with pkgs;
       {
         devShells.default = mkShell {
-          buildInputs = [
-            pkgs.zigpkgs.master
+          buildInputs = with pkgs; [
+            zigpkgs."0.11.0"
+            zls
           ];
         };
       }
