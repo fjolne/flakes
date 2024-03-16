519b7aca0ba352531a660b63c9b730bfbdc1243d
Oleg Martynov <fjolne.yngling@gmail.com>
2024-03-16 16:27:01 +0000

    python-raw2: experiments

--- python-raw2/flake.nix
+++ python-raw2/flake.nix
@@ -16,23 +16,40 @@
           #   cudaSupport = true;
           # };
         };
-        python = pkgs.python311;
         libs = with pkgs; lib.makeLibraryPath [
           zlib # numpy
           stdenv.cc.cc.lib # torch
           addOpenGLRunpath.driverLink # torch.cuda
           # cudaPackages.libcublas.lib
         ];
+        py = pkgs.python311;
+        python = pkgs.symlinkJoin {
+          name = py.name + "-wow";
+          paths = [
+            (pkgs.writeShellScriptBin "python3.11" ''
+              export LD_LIBRARY_PATH=${libs}
+              exec ${py}/bin/python3.11 "$@"
+            '')
+            py
+          ];
+        };
+        venv = ".venv";
       in
       {
         devShells = {
           default = pkgs.mkShell {
             packages = [
-              (pkgs.writeShellScriptBin "python" ''
-                export LD_LIBRARY_PATH=${libs}
-                exec ${python}/bin/python "$@"
-              '')
+            (pkgs.writeShellScriptBin "python" ''
+              export LD_LIBRARY_PATH=${libs}
+              exec ${py}/bin/python3.11 "$@"
+            '')
             ];
+            shellHook = ''
+              rsync -a ${py}/ .venv && chmod -R 755 .venv
+              # [[ ! -e ${venv} ]] && ${py}/bin/python -m venv ${venv}
+              #                  # && ln -sf ${py}/bin/python3.11 ${venv}/bin/python3.11
+              # # . .venv/bin/activate
+            '';
           };
         };
       });
