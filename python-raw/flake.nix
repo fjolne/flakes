f09e123f5ec477a40466e789acd77eaa48cf04b8
Oleg Martynov <fjolne.yngling@gmail.com>
2024-04-07 18:15:56 +0000

    added python-raw with no deps

---
+++ python-raw/flake.nix
@@ -0,0 +1,26 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system}; in
+      {
+        devShells = {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              poetry
+            ];
+            LD_LIBRARY_PATH = with pkgs; lib.makeLibraryPath [
+              # zlib # numpy
+            ];
+            shellHook = ''
+              poetry env use ${pkgs.python311}/bin/python
+              poetry install --no-root
+            '';
+          };
+        };
+      });
+}
