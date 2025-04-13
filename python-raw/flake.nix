2e7729a3c29f051a6789bb431b241e8d82bca6ef
Oleg Martynov <fjolne.yngling@gmail.com>
2025-04-13 15:20:12 +0100

    python-raw: updated

--- python-raw/flake.nix
+++ python-raw/flake.nix
@@ -1,29 +1,44 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, nixpkgs-unstable, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = nixpkgs.legacyPackages.${system};
-        python = pkgs.python313;
+        pkgs = import nixpkgs {
+          inherit system;
+          config = {
+            allowUnfree = true;
+          };
+        };
+        pkgs-unstable = import nixpkgs-unstable {
+          inherit system;
+          config = {
+            allowUnfree = true;
+          };
+        };
+        python = pkgs.python312;
       in
       {
         devShells = with pkgs; {
           default = mkShell {
             packages = [
-              poetry
+              pkgs-unstable.poetry
             ];
-            LD_LIBRARY_PATH = (lib.makeLibraryPath [
-              zlib
-              stdenv.cc.cc.lib
-            ]);
             shellHook = ''
-              poetry env use ${python}/bin/python
-              poetry install --no-root
-              export PYTHONPATH=$(pwd):$PYTHONPATH;
+              export LD_LIBRARY_PATH=${lib.makeLibraryPath [
+                zlib
+                stdenv.cc.cc.lib
+                libGL
+                glib
+              ]}:$LD_LIBRARY_PATH
+
+              [[ ! -d .venv ]] && poetry env use ${python}/bin/python && poetry install --no-root
+              . $(dirname $(poetry env info --executable))/activate
+              export PYTHONPATH=$(pwd):$PYTHONPATH
             '';
           };
         };
